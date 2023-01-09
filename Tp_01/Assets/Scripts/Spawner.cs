5240ed938d3c2fab45046ef76f1c27d0d755e033
Victorine Cassé <victorine.casse@telecom-sudparis.eu>
2023-01-09 13:15:51 +0100

    start of the spawners

---
+++ Tp_01/Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public TroupFactory troupFactory;
+    [SerializeField] private float SpawnDelay = 0.5f;
+    private float timer;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > SpawnDelay) {
+            troupFactory.SpawnTroup();
+            timer = 0f;
+        }
+    }
+}
---
+++ Tp_01/Assets/Scripts/TroupFactory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class TroupFactory:MonoBehaviour
+{
+    [SerializeField] Transform[] spawnZones;
+    Transform selectedSpawn;
+    int index;
+    [SerializeField] GameObject[] enemies;
+    GameObject enemy;
+    int index2;
+
+    void Start()
+    {
+        index = Random.Range(0, spawnZones.Length);
+        selectedSpawn = spawnZones[index];
+        index2 = Random.Range(0, enemies.Length);
+        enemy = enemies[index2];
+    }
+
+    public void SpawnTroup()
+    {
+        Instantiate(enemy, selectedSpawn.position, selectedSpawn.rotation);
+    }
+}
