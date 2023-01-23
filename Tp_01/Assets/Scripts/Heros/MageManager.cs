b642a8475150ffd933f86e8c7dc0789f710ea355
jondew <jon.wengde@gmail.com>
2023-01-23 14:34:02 +0100

    Previous mage animator settings restored

--- Tp_01/Assets/Scripts/Heros/MageManager.cs
+++ Tp_01/Assets/Scripts/Heros/MageManager.cs
@@ -31,7 +31,9 @@ public class MageManager : PlayerManager
 
     protected override void Action1()
     {
+        if (onCooldown1) return; //REQUIRED ON ALL CHARACTER ACTIONS to avoid animation state locking
         //Start fireball sequence:
+        onCooldown1 = true;
         animator.SetBool("Fireball", true);
     }
 
@@ -43,7 +45,10 @@ public class MageManager : PlayerManager
 
     protected override void Action2()
     {
+        if (onCooldown2) return; //REQUIRED ON ALL CHARACTER ACTIONS to avoid animation state locking
+        onCooldown2 = true;
         animator.SetBool("Orb", true);
+
     }
 
     void PlayerReleaseOrb()
@@ -53,6 +58,8 @@ public class MageManager : PlayerManager
 
     protected override void Action3()
     {
+        if (onCooldown3) return; //REQUIRED ON ALL CHARACTER ACTIONS to avoid animation state locking
+        onCooldown3 = true;
         animator.SetBool("Wall", true);
     }
     
